f7666decf05834e86387b7ad5bcc921503a45ebd
Claudiu Moise <claudiu.moise@gmail.com>
2018-04-24 16:07:10 -0500

    CSS updates

--- home.php
+++ home.php
@@ -20,7 +20,7 @@
             +"</td><td><? $_SESSION['id']="+allParties[i].partyID
             +"?> <form method='get' action='characters.php'><input  name='id' type='hidden' value="
             +allParties[i].partyID
-            +"><input type='submit' value='List of Characters'></form></td><td><input type='button'  class='deleteBtn' id='"+allParties[i].partyID+"' value='Delete'></td> ";
+            +"><input type='submit' value='List of Characters' class='charRedirForm button'></form></td><td><input type='button'  class='deleteBtn button' id='"+allParties[i].partyID+"' value='Delete'></td> ";
           party="<tr id='"+allParties[i].partyID+"'>"+party+"</tr>";
           $("#myitemstable").append(party);
         }
@@ -58,19 +58,21 @@
       });
     });
     </script>
+
+    <link href="css/home.css" rel="stylesheet" type="text/css" media="screen"/>
   </head>
   <body id='bodytag'>
-    <div>   
+    <div id="logindiv">   
       <form method="post" action="RPGservices.php">
         Username: <input type="text" name="uNameBox">
         Password: <input type="password" name="pWordBox">
-        <input type="submit">
+        <input class="button" type="submit">
       </form>
     </div>
-    <div id='tableinfo'>
+    <div id='tablediv'>
       <h1>RPG Parties </h1>
       <ul id="myitems"></ul>
-      <table id='myitemstable'style='width:60%; text-align:center;' border='2px'>
+      <table id='myitemstable'>
         <tr>
           <th>Party ID</th>
           <th>Name</th>
@@ -78,7 +80,7 @@
         <tr>
           <td></td>
           <td><input type="text" id="name"/></td>
-          <td><input type="button" id="saveitem" value="Save Item"/></td>
+          <td><input type="button" id="saveitem" class="button" value="Save Item"/></td>
           <td></td>
         </tr>
       </table>
