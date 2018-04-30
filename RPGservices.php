593dc0799137765564c4195a3fb1e15c3667d0dd
mcortes95 <marcocortes95@protonmail.com>
2018-04-30 13:32:52 -0500

    fixed posting to db

--- RPGservices.php
+++ RPGservices.php
@@ -19,11 +19,6 @@
   if($method=="GET"){
 
     $result=mysqli_query($conn,$_SESSION['query']);
-=======
-    //$colm=$_GET['colm'];
-    //$tabl=$_GET['tabl']
-    $sql="SELECT * FROM party";
-    $result=mysqli_query($conn,$sql);
     $rows=array();
     if(mysqli_num_rows($result)>0){
       while($r=mysqli_fetch_assoc($result)){
@@ -37,47 +32,29 @@
   }
 
   else if($method=="POST"){
-//     //defining insert method
-//     $sql_insert=$_POST['vName'];
-//     //echo $sql_insert;
-//     $result=mysqli_query($conn,$sql_insert);
-    
-//     if(!$result){
-//       //echo "YOUR QUERY IS FUCKED";
-//     }
-//     if($result){
-//       //echo "YOUR QUERY IS WORKING";
-//     }
-//     //echo $result;
-//     //echo mysqli_num_rows($result);
-//     if(substr($sql_insert,0,6)=='SELECT'){
-//       //echo "not inserting into table";
-//       if(mysqli_num_rows($result)>0){
-//         while($row=mysqli_fetch_assoc($result)){
-//           if(substr_count($sql_insert, "party")>0){
-//             echo $row['partyName'];
-//           }
-//           elseif (substr_count($sql_insert, "characters")>0) {
-//             echo $row['charName']; 
-//           }
-          
-//         } 
-//       }
-//     }
-//     /*
-//     else{
-//       echo "whoops, youre a fuckhead";
-//       //echo " ERROR: $sql_insert did not run. ".mysqli_error($conn);
-//     }*/
-    
-
-    $name=$_POST['partyName'];
+    /*$name=$_POST['partyName'];
     $sql_insert="INSERT INTO party(partyName) VALUES ('$name')";
     if(mysqli_query($conn,$sql_insert)){
       echo "Items succesfully added to the database.";
     }
     else{
       echo "ERROR: $sql_insert did not run. ".mysqli_error($conn);
+    }*/
+    $sql_insert=$_POST['vName'];   
+    $result=mysqli_query($conn,$sql_insert);
+    if(substr($sql_insert,0,6)=='SELECT'){
+      //echo "not inserting into table";
+      if(mysqli_num_rows($result)>0){
+        while($row=mysqli_fetch_assoc($result)){
+          if(substr_count($sql_insert, "party")>0){
+            echo $row['partyName'];
+          }
+          elseif (substr_count($sql_insert, "characters")>0) {
+            echo $row['charName']; 
+          }
+          
+        } 
+      }
     }
   }
 
